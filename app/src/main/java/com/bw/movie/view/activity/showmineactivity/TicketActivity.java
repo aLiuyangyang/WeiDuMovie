09339d376fb8585afe51b0ea46466f43f0aad5a8
liuyangyang <liuyangyang@qq.com>
2019-02-15 10:06:05 +0800

    Merge branch 'lyy' of https://github.com/aLiuyangyang/WeiDuMovie into lyy
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/bw/movie/view/activity/showmineactivity/TicketActivity.java
+++ app/src/main/java/com/bw/movie/view/activity/showmineactivity/TicketActivity.java
@@ -1,15 +1,54 @@
 package com.bw.movie.view.activity.showmineactivity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.bw.movie.R;
+import com.bw.movie.base.BaseActivity;
 
-public class TicketActivity extends AppCompatActivity {
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class TicketActivity extends BaseActivity {
+
+    @BindView(R.id.ticket_obligation)
+    RadioButton ticketObligation;
+    @BindView(R.id.ticket_stocks)
+    RadioButton ticketStocks;
+    @BindView(R.id.ticket_radio)
+    RadioGroup ticketRadio;
+    @BindView(R.id.ticket_viewpager)
+    ViewPager ticketViewpager;
+    private List<Fragment> list;
+    @Override
+    public void initView() {
+        ButterKnife.bind(this);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ticket);
+    public void initData() {
+
+    }
+
+    @Override
+    public int getContent() {
+        return R.layout.activity_ticket;
+    }
+
+    @Override
+    public void success(Object data) {
+
+    }
+
+    @Override
+    public void fail(String error) {
+
     }
 }
